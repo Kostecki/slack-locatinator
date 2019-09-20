fe157f19c3762e7736a81ce236a48e67e0f2d1b1
Jacob Kostecki <kostecki.jacob@gmail.com>
2019-09-20 13:17:02 +0200

    remove config-file from repo..

--- config.js
+++
@@ -1,4 +0,0 @@
-var config = {
-  mapBoxToken: 'pk.eyJ1Ijoiamtvc3RlY2tpIiwiYSI6ImNrMHJ3ZWR3cDA5eHQzbG85bHptdmlzaHoifQ.mYZ-Sc_VQIhHB0bWWDaEOQ',
-  slackWebhookURL: 'https://hooks.slack.com/services/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'
-}
