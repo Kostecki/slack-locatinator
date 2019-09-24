1963d117f41d7532333b4c5ac0da9b6089c26fcd
Jacob Kostecki <kostecki.jacob@gmail.com>
2019-09-24 10:14:41 +0200

    update node modules

--- api/routes/gps.js
+++ api/routes/gps.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
 router.post('/', function(req, res, next) {
   let sender = req.body.user_name;
   let channelId = req.body.channel_id;
