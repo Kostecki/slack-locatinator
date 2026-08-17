fdb3b20040c00b3e91c7f8278e9f3950563390e5
Jacob Kostecki <jacob@kostecki.dk>
2026-08-17 13:03:15 +0200

    fix

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   output: "standalone",
+  outputFileTracingIncludes: {
+    "/**": ["./node_modules/@swc/helpers/**"],
+  },
   experimental: {
     useTypeScriptCli: true,
   },
